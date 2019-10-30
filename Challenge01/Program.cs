7a55b9cd0b2fb86387f14f18415024919fb77d5f
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 23:32:15 +0700

    Update first challenge

--- Challenge01/Program.cs
+++ Challenge01/Program.cs
@@ -7,6 +7,8 @@ namespace YourName
         static void Main(string[] args)
         {
             // Get your name by using Console.ReadLine()
+            // Example output: "Hello, my name is Salamander.",
+            // depends on user input.
 
             Console.WriteLine("Hello, my name is {0}.");
         }
