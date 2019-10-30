a14562d24a3bd1b663cc6b07c26022020772edac
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 21:17:25 +0700

    Update Challenge

--- Challenge04/Program.cs
+++ Challenge04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Substitution
 {
@@ -6,7 +7,18 @@ namespace Substitution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // We have some word collection ...
+            var censoredWords = new ArrayList();
+            censoredWords.Add("imperdiet");
+            censoredWords.Add("dolor");
+            censoredWords.Add("duo");
+
+            // and a paragraph.
+            var paragraph = "Lorem ipsum dolor sit amet, imperdiet vituperata duo in, nonumy.";
+
+            // Censor the words in paragraph by using * symbol.
+            // Output example:
+            // Lorem ipsum ***** sit amet, ********* vituperata *** in, nonumy.
         }
     }
 }
