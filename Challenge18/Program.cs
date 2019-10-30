dcc33b1645cab4d4ea0aed06f291d98507100d86
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 23:22:59 +0700

    Max, min, average challenge

--- Challenge18/Program.cs
+++ Challenge18/Program.cs
@@ -6,7 +6,26 @@ namespace FindMinMaxAvg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = {2, 4, 54, 12, -65, 19, 40, 92, 88, 330, -4, 54};
+
+            FindMax(numbers); // Output: 330
+            FindMin(numbers); // Output: -65
+            FindAverage(numbers); // Output: 52.16
+        }
+
+        static int FindMax(int[] numbers)
+        {
+            return 0;
+        }
+
+        static int FindMin(int[] numbers)
+        {
+            return 0;
+        }
+
+        static int FindAverage(int[] numbers)
+        {
+            return 0;
         }
     }
 }
