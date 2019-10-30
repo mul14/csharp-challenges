ffcd4e8d13adee6643481259dc32314f8e9f1378
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 22:50:54 +0700

    Add command line argument input challenge

--- Challenge11/Program.cs
+++ Challenge11/Program.cs
@@ -6,7 +6,9 @@ namespace SumArgument
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Sum all args
+            // You can run "dotnet run -- 1 5 8 3"
+            // The output should be 17
         }
     }
 }
