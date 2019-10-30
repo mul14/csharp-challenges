645f16992c76ef73fcd98bcf42a4e6f56735007c
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 22:59:33 +0700

    Add counting words challenge

--- Challenge13/Program.cs
+++ Challenge13/Program.cs
@@ -6,7 +6,15 @@ namespace CountWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountWords("Hello"); // Output: 1
+            CountWords("Hello World"); // Output: 2
+            CountWords("Lorem ipsum dolor sit amet"); // Output: 5
+        }
+
+        static int CountWords(string value)
+        {
+            // Make this method to be able counting words
+            return 0;
         }
     }
 }
