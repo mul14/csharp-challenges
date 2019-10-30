11a22a1fbcbf671ba56753c7a62b35ad8378e4e0
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 20:58:25 +0700

    3rd Challenge

--- Challenge03/Program.cs
+++ Challenge03/Program.cs
@@ -6,7 +6,12 @@ namespace StringLength
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create new method called "CountString".
+            // That method should accept a string and will
+            // return length of characters input.
+
+            Console.WriteLine("'Hello World!' have {0} characters", CountString("Hello World!"));
+            Console.WriteLine("'Hello Universe!' have {0} characters", CountString("Hello Universe!"));
         }
     }
 }
