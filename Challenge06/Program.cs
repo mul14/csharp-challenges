4f1348db44df14c7677af175b56f4255185bfe48
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-10-30 21:26:35 +0700

    Update Challenge

--- Challenge06/Program.cs
+++ Challenge06/Program.cs
@@ -6,7 +6,13 @@ namespace Grade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a method called Grade()
+            // That method should accept an integer with some criteria
+            // 1) If the input more than 90 the output will be "A"
+            // 2) If the input within range 80 to 89 the output will be "B"
+            // 3) If the input within range 70 to 79 the output will be "C"
+            // 4) If the input within range 60 to 69 the output will be "D"
+            // 5) If the input below 59 the output will be "E"
         }
     }
 }
