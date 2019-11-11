a83cedcc5494edee637b188948781dd018fca783
Mulia Nasution <mul14@users.noreply.github.com>
2019-11-11 14:55:29 +0700

    Update Program.cs

--- Challenge06/Program.cs
+++ Challenge06/Program.cs
@@ -8,7 +8,7 @@ namespace Grade
         {
             // Create a method called Grade()
             // That method should accept an integer with some criteria
-            // 1) If the input more than 90 the output will be "A"
+            // 1) If the input equal or more than 90 the output will be "A"
             // 2) If the input within range 80 to 89 the output will be "B"
             // 3) If the input within range 70 to 79 the output will be "C"
             // 4) If the input within range 60 to 69 the output will be "D"
